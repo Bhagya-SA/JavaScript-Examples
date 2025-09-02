a4c6978f97588e826aaf9f79180a512eff0c580e
Bhagya-SA <bhagyaanjaneya@gmail.com>
2025-09-02 19:23:22 +0530

    Create operators.js

---
+++ 01_Basics/operators.js
@@ -0,0 +1,24 @@
+// JavaScript Operators
+
+let a = 10;
+let b = 5;
+
+// Arithmetic Operators
+console.log("a + b =", a + b);
+console.log("a - b =", a - b);
+console.log("a * b =", a * b);
+console.log("a / b =", a / b);
+console.log("a % b =", a % b);
+
+// Comparison Operators
+console.log("a == b:", a == b);
+console.log("a != b:", a != b);
+console.log("a > b:", a > b);
+console.log("a <= b:", a <= b);
+
+// Logical Operators
+let x = true;
+let y = false;
+console.log("x && y:", x && y);
+console.log("x || y:", x || y);
+console.log("!x:", !x);
