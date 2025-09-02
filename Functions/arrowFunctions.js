ca096104c09d718f6712904432b951315e0e29cd
Bhagya-SA <bhagyaanjaneya@gmail.com>
2025-09-02 19:27:43 +0530

    Create arrowFunctions.js

---
+++ Functions/arrowFunctions.js
@@ -0,0 +1,15 @@
+// Arrow functions in JavaScript (ES6)
+
+// Arrow function to greet a person
+const greet = (name) => "Hello, " + name + "!";
+
+// Arrow function to add two numbers
+const add = (a, b) => a + b;
+
+// Using the arrow functions
+console.log(greet("Bhagya"));          // Hello, Bhagya!
+console.log("Sum:", add(20, 30));      // Sum: 50
+
+// Arrow function with no parameter
+const sayHello = () => "Hello World!";
+console.log(sayHello());
