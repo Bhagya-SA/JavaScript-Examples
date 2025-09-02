bed386cac5ed0745942d3d07bb705002674fbadf
Bhagya-SA <bhagyaanjaneya@gmail.com>
2025-09-02 19:26:03 +0530

    Create loops.js

---
+++ ControlFlow/loops.js
@@ -0,0 +1,23 @@
+// Loops in JavaScript
+
+// For loop
+console.log("For loop:");
+for (let i = 1; i <= 5; i++) {
+    console.log(i);
+}
+
+// While loop
+console.log("\nWhile loop:");
+let count = 3;
+while (count > 0) {
+    console.log(count);
+    count--;
+}
+
+// Do-While loop
+console.log("\nDo-While loop:");
+let num = 1;
+do {
+    console.log(num);
+    num++;
+} while (num <= 3);
