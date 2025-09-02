ec4ce6325fb5b28484779b8bb3761253c048ad5f
Bhagya-SA <bhagyaanjaneya@gmail.com>
2025-09-02 19:21:54 +0530

    Create variables1.js

---
+++ 01_Basics/variables1.js
@@ -0,0 +1,17 @@
+// Variables in JavaScript
+
+// Using let (can be changed)
+let name = "Bhagya";
+console.log("Name:", name);
+
+// Using const (cannot be changed)
+const pi = 3.1416;
+console.log("PI value:", pi);
+
+// Using var (older way, not recommended)
+var age = 21;
+console.log("Age:", age);
+
+// Reassigning let variable
+name = "John";
+console.log("Updated Name:", name);
