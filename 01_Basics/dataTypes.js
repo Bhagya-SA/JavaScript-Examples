f3a5301a5a37d54b12376fd254eedcb3f0228381
Bhagya-SA <bhagyaanjaneya@gmail.com>
2025-09-02 19:22:45 +0530

    Create dataTypes.js

---
+++ 01_Basics/dataTypes.js
@@ -0,0 +1,25 @@
+// JavaScript Data Types
+
+// Number
+let age = 21;
+console.log("Age:", age, "- Type:", typeof age);
+
+// String
+let name = "Bhagya";
+console.log("Name:", name, "- Type:", typeof name);
+
+// Boolean
+let isStudent = true;
+console.log("Is Student:", isStudent, "- Type:", typeof isStudent);
+
+// Undefined
+let city;
+console.log("City:", city, "- Type:", typeof city);
+
+// Null
+let job = null;
+console.log("Job:", job, "- Type:", typeof job);
+
+// Object
+let student = { name: "Bhagya", age: 21 };
+console.log("Student:", student, "- Type:", typeof student);
