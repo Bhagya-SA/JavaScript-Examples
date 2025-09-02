e625fb9a41f5c9175b5491e81c098dfe371f4d9c
Bhagya-SA <bhagyaanjaneya@gmail.com>
2025-09-02 19:27:13 +0530

    Create functions.js

---
+++ Functions/functions.js
@@ -0,0 +1,15 @@
+// Normal function in JavaScript
+
+// Function to greet a person
+function greet(name) {
+    return "Hello, " + name + "!";
+}
+
+// Function to add two numbers
+function add(a, b) {
+    return a + b;
+}
+
+// Using the functions
+console.log(greet("Bhagya"));          // Hello, Bhagya!
+console.log("Sum:", add(10, 5));       // Sum: 15
