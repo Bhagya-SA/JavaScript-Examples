c146be77fefae9139b232969620e2a059a3fc0d4
Bhagya-SA <bhagyaanjaneya@gmail.com>
2025-09-02 19:29:04 +0530

    Create arrays.js

---
+++ Arrays/arrays.js
@@ -0,0 +1,15 @@
+// Creating and accessing arrays in JavaScript
+
+// Array of fruits
+let fruits = ["Apple", "Banana", "Mango", "Orange"];
+
+// Accessing elements
+console.log("First fruit:", fruits[0]);   // Apple
+console.log("All fruits:", fruits);       // ["Apple", "Banana", "Mango", "Orange"]
+
+// Modifying elements
+fruits[1] = "Grapes";
+console.log("Updated fruits:", fruits);
+
+// Array length
+console.log("Number of fruits:", fruits.length);
